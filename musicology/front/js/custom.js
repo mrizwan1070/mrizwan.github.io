13bfc32e55c2303e286528cf6a0d5ebb23d83f8e
mrizwan1070 <109618918+mrizwan1070@users.noreply.github.com>
2022-09-09 12:31:02 +0500

    required link added to header

---
+++ musicology/front/js/custom.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function () {
+    var $ = jQuery;
+
+    $(document).on("click", "#sidebarprd-next", function () {
+        $("#carouselhomesidebar").carousel("next");
+    });
+
+    $(document).on("click", "#sidebarprd-prev", function () {
+        $("#carouselhomesidebar").carousel("prev");
+    });
+});
+
+$(document).on("click", ".change_country", function (e) {
+    var _token = $('meta[name="csrf-token"]').attr("content");
+    var data = {
+        country_id: this.id,
+        _token: _token,
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "/changecountry",
+        data: data,
+        success: function (data_response) {
+            setTimeout(function () {
+                window.location.reload();
+            }, 1);
+            // if (data_response == "success") {
+            //
+            // }
+        },
+        complete: function (data_response) {},
+    });
+});
+
+$(document).on("submit", "#top_search_form", function (e) {
+    e.preventDefault();
+    var form = $(this).serialize();
+    console.log(form);
+});
