d236555151d46bef7a97c6e9de38315adbab603e
Muhammad Rizwan <mrizwan1070@gmail.com>
2023-01-25 12:15:54 +0500

    new branch

--- grocery/front/js/custom.js
+++ grocery/front/js/custom.js
@@ -1,55 +1,5 @@
-jQuery(document).ready(function () {
-    var $ = jQuery;
-
-    $(document).on("click", "#sidebarprd-next", function () {
-        $("#carouselhomesidebar").carousel("next");
+$(document).ready(function (){
+    $(".search-toggle").click(function(){
+        $(".main-search").toggleClass("d-none");
     });
-
-    $(document).on("click", "#sidebarprd-prev", function () {
-        $("#carouselhomesidebar").carousel("prev");
-    });
-    $("#grid").click(function(){
-        $("#products .row > div").addClass("listing-group");
-        $("#grid").addClass("active");
-        $("#list").removeClass("active");
-    })
-    $("#list").click(function(){
-        $("#products .row > div").removeClass("listing-group");
-        $("#grid").removeClass("active");
-        $("#list").addClass("active");
-    })
-    $(".section-title .icon-change").click(function(){
-        const icon = $(this).find("i");
-        icon.toggleClass("fa-plus");
-        
-    })
-});
-
-$(document).on("click", ".change_country", function (e) {
-    var _token = $('meta[name="csrf-token"]').attr("content");
-    var data = {
-        country_id: this.id,
-        _token: _token,
-    };
-
-    $.ajax({
-        type: "POST",
-        url: "/changecountry",
-        data: data,
-        success: function (data_response) {
-            setTimeout(function () {
-                window.location.reload();
-            }, 1);
-            // if (data_response == "success") {
-            //
-            // }
-        },
-        complete: function (data_response) {},
-    });
-});
-
-$(document).on("submit", "#top_search_form", function (e) {
-    e.preventDefault();
-    var form = $(this).serialize();
-    console.log(form);
 });
