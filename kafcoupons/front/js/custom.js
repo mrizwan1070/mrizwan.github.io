ae3d364c15c0cbddb522d13ba509492f65b5eb32
Muhammad Rizwan <mrizwan1070@gmail.com>
2024-01-26 15:23:57 +0500

    latest code

--- kafcoupons/front/js/custom.js
+++ kafcoupons/front/js/custom.js
@@ -4,11 +4,35 @@ jQuery(document).ready(function () {
     slidesToShow: 10,
     slidesToScroll: 1,
     autoplay: true,
+    infinite: true,
     autoplaySpeed: 0,
     speed: 1000,
     pauseOnFocus: false, 
     pauseOnHover: true,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            infinite: true,
+          }
+        }
+    ]
 });
 $('.category-cards').slick({
     slidesToShow: 2,
@@ -19,6 +43,22 @@ $('.category-cards').slick({
     pauseOnFocus: false, 
     pauseOnHover: true,
     cssEase: 'linear',
-    arrows: false
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            infinite: true,
+          }
+        },
+        // {
+        //   breakpoint: 480,
+        //   settings: {
+        //     slidesToShow: 2,
+        //     infinite: true,
+        //   }
+        // }
+    ]
 });
 });
