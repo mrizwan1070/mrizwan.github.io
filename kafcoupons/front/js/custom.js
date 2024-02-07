529da455620ed9bf4b18ecc2b71420f6e7348d08
Muhammad Rizwan <mrizwan1070@gmail.com>
2024-02-07 18:33:47 +0500

    code updated

--- kafcoupons/front/js/custom.js
+++ kafcoupons/front/js/custom.js
@@ -1,4 +1,16 @@
 jQuery(document).ready(function () {
+    //star rating bars
+    $('.rating-bar').each(function() {
+        // Get the total-stars attribute value
+        var totalStars = parseInt($(this).find('.rate-bar').attr('total-stars'));
+
+        // Calculate the width for the rating bar
+        var widthPercentage = (totalStars / 100) * 100; // Assuming the maximum value is 100
+
+        // Set the width using CSS
+        $(this).find('.rate-bar').css('width', totalStars + '%');
+    });
+    $(".cb-status.cb-fill").parents(".col").prevAll().find(".cb-status").addClass("cb-fill");
  // brand slider
 //  $('.brand-slider').slick({
 //     slidesToShow: 10,
@@ -68,10 +80,30 @@ $('.brand-slider').owlCarousel({
     loop:true,
     margin:1,
     autoplay:true,
-    autoplayTimeout:1000,
+    autoplayTimeout:2000,
+    autoplaySpeed: 2000,
+    slideTransition: 'linear',
     autoplayHoverPause:true,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn'
+    responsive:{
+        1200 : {
+            items: 10 
+        },
+        800 : {
+            items: 8 
+        },
+        768 : {
+            items: 7 
+        },
+        576 : {
+            items: 5 
+        },
+        480 : {
+            items: 4 
+        },
+        0 : {
+            items: 2
+        }
+    }
 });
 $('.category-cards').owlCarousel({
     items:3,
@@ -79,9 +111,36 @@ $('.category-cards').owlCarousel({
     margin:1,
     autoplay:true,
     slideTransition: 'linear',
-    autoplayTimeout:2000,
+    autoplayTimeout:10000,
+    autoplaySpeed: 10000,
     autoplayHoverPause:true,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn'
+    responsive : {
+        0 : {
+            items : 1
+        },
+        480 : {
+            items : 2
+        }
+    }
+});
+$('.power-cb-cards').owlCarousel({
+    items:3,
+    loop:true,
+    margin:1,
+    nav: true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items: 1
+        },
+        576:{
+            items: 2
+        },
+       900: {
+        items: 3
+       }
+    }
 });
 });
