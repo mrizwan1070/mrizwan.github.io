cf7d98cc4921410296ae9e3c1d4283088af56d2f
Muhammad Rizwan <mrizwan1070@gmail.com>
2024-04-04 10:27:09 +0500

    mobile view updated

--- kafcoupons/front/js/custom.js
+++ kafcoupons/front/js/custom.js
@@ -70,70 +70,6 @@ jQuery(document).ready(function () {
     $('#verification').on('click', function(){
         $('#userRegister').css('display', 'block');
     });
- // brand slider
-//  $('.brand-slider').slick({
-//     slidesToShow: 10,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     infinite: true,
-//     draggable: true,
-//     autoplaySpeed: 0,
-//     speed: 1000,
-//     pauseOnFocus: false, 
-//     pauseOnHover: true,
-//     cssEase: 'linear',
-//     responsive: [
-//         {
-//           breakpoint: 1024,
-//           settings: {
-//             slidesToShow: 5,
-//             infinite: true,
-//           }
-//         },
-//         {
-//           breakpoint: 600,
-//           settings: {
-//             slidesToShow: 3,
-//             infinite: true,
-//           }
-//         },
-//         {
-//           breakpoint: 480,
-//           settings: {
-//             slidesToShow: 2,
-//             infinite: true,
-//           }
-//         }
-//     ]
-// });
-// $('.category-cards').slick({
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     draggable: true,
-//     autoplaySpeed: 0,
-//     speed: 13000,
-//     pauseOnFocus: false, 
-//     pauseOnHover: true,
-//     cssEase: 'linear',
-//     arrows: false,
-//     responsive: [
-//         {
-//           breakpoint: 600,
-//           settings: {
-//             slidesToShow: 1,
-//             infinite: true,
-//           }
-//         },
-//         // {
-//         //   breakpoint: 480,
-//         //   settings: {
-//         //     slidesToShow: 2,
-//         //     infinite: true,
-//         //   }
-//         // }
-//     ]
-// });
 $('.brand-slider').owlCarousel({
     items:10,
     loop:true,
@@ -160,7 +96,7 @@ $('.brand-slider').owlCarousel({
             items: 4 
         },
         0 : {
-            items: 2
+            items: 3
         }
     }
 });
